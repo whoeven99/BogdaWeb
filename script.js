b3abdd87414a7e3eafb3ffa6805082a8daf67e9c
whoeven <>
2025-07-23 17:13:11 +0800

    init

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.getElementById("clickMe").addEventListener("click", function() {
-    document.getElementById("message").textContent = "Button clicked! 🎉";
+    document.getElementById("message").textContent = "Button clicked!";
 });
