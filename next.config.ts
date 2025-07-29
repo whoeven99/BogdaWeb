aabb4beacedbac7cf0615692dcbe4b69cd7a0b64
liuwenjie837 <15159210793@163.com>
2025-07-29 16:49:57 +0800

    title color

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export', // 开启静态导出
+  images: {
+    unoptimized: true, // 关闭图片优化
+  },
 };
 
 export default nextConfig;
