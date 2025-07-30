d6cac6575f3acb85497e2ed9cd15e770e6c325ed
liuwenjie837 <15159210793@163.com>
2025-07-30 11:08:08 +0800

    deploy help-center

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,17 @@
 import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export', // 开启静态导出
+  output: "export", // 开启静态导出
   images: {
     unoptimized: true, // 关闭图片优化
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/help-center/:path*",
+        destination: "/help-center/index.html",
+      },
+    ];
   },
 };
 
---
+++ public/help-center/zh-Hans/assets/js/a7456010.b9c900e7.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkhelp_center=self.webpackChunkhelp_center||[]).push([[235],{8552:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
