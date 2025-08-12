84e95326c7e95ae3649a6e760e01dc613716ab7e
liuwenjie837 <15159210793@163.com>
2025-08-12 17:29:52 +0800

    优化官网

--- ciwi-web/src/app/[slug]/page.tsx
+++ ciwi-web/src/app/[slug]/page.tsx
@@ -1,18 +1,23 @@
 import { footerItemsMap } from "@/data/footerItems";
 import styles from '../css/singlePage.module.css'
 import { notFound } from "next/navigation"; // 导入 notFound
+
+// 导入 Next.js 的 PageProps 类型
+import PageProps from "next";
+
 export function generateStaticParams() {
   return Object.keys(footerItemsMap).map((slug) => ({
     slug,
   }));
 }
-interface PageProps {
+// 扩展 Next.js 的 PageProps 类型
+interface CustomPageProps extends Omit<typeof PageProps, "params"> {
   params: {
     slug: string;
   };
 }
 
-export default function Page({ params }: PageProps) {
+export default function Page({ params }: CustomPageProps) {
   const data = footerItemsMap[params.slug as keyof typeof footerItemsMap];
     console.log(data);
     
