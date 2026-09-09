1b2ac2cac5318568a45870fe519afdcaaf9098d6
whoeven99 <125242644+whoeven99@users.noreply.github.com>
2026-09-09 08:15:54 +0800

    Revert "Allen"

--- ciwi-web/src/app/page.tsx
+++ ciwi-web/src/app/page.tsx
@@ -1,4 +1,3 @@
-import {AffiliateLanding} from "@/components/affiliate/AffiliateLanding";
 import {FinalCtaSection} from "@/components/sections/FinalCtaSection";
 import {FaqSection} from "@/components/sections/FaqSection";
 import {HeroSection} from "@/components/sections/HeroSection";
@@ -7,7 +6,6 @@ import {ProductMatrixSection} from "@/components/sections/ProductMatrixSection";
 import {ResourcesSection} from "@/components/sections/ResourcesSection";
 import {SocialProofSection} from "@/components/sections/SocialProofSection";
 import {PageContainer} from "@/components/ui/PageContainer";
-import {getAffiliateLanding} from "@/content/affiliate";
 import {getHomePageCopy} from "@/content/home-page-copy";
 import {getProducts} from "@/content/products";
 import {getUiCopy} from "@/content/ui-copy";
@@ -28,25 +26,8 @@ export async function generateMetadata() {
   });
 }
 
-type HomePageProps = {
-  searchParams?: Promise<{ref?: string; product?: string}>;
-};
-
-export default async function HomePage({searchParams}: HomePageProps) {
+export default async function HomePage() {
   const locale = await getRequestLocale();
-  const params = await searchParams;
-  const isAffiliate = Boolean(params?.ref);
-
-  if (isAffiliate) {
-    return (
-      <main>
-        <PageContainer>
-          <AffiliateLanding data={getAffiliateLanding(locale)} />
-        </PageContainer>
-      </main>
-    );
-  }
-
   const copy = getHomePageCopy(locale);
   const uiCopy = getUiCopy(locale);
   const products = getProducts(locale);
